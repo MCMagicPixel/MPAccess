c9c8be4471c9c547075201eb5180b0951ea86531
PlaidMrdeer <98594477+PlaidMrdeer@users.noreply.github.com>
2024-04-19 16:14:19 +0800

    第一次提交

---
+++ src/main/java/network/magicpixel/bukkit/MPAccess.java
@@ -0,0 +1,17 @@
+package network.magicpixel.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author PlaidMrdeer
+ */
+
+public final class MPAccess extends JavaPlugin
+{
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
